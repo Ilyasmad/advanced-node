0be76c77167ff330557ea28dc03d2a7f22a88b18
Ahmad Ilyas Roose <43195364+Ilyasmad@users.noreply.github.com>
2018-11-10 17:07:11 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const session     = require('express-session');
+const passport    = require('passport');
+const mongo       = require('mongodb').MongoClient;
+const ObjectID    = require('mongodb').ObjectID;
+const LocalStrategy = require('passport-local');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const routes = require('./routes.js');
+const auth = require('./auth.js');
+
+const app = express();
+
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'pug')
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongo.connect(process.env.DATABASE, (err, db) => {
+    if(err) {
+        console.log('Database error: ' + err);
+    } else {
+        console.log('Successful database connection');
+      
+        auth(app, db);
+        routes(app, db);
+
+        app.use((req, res, next) => {
+          res.status(404)
+            .type('text')
+            .send('Not Found');
+        });
+
+        app.listen(process.env.PORT || 3000, () => {
+          console.log("Listening on port " + process.env.PORT);
+        });  
+}});
